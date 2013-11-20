fab55359d57720b304f725c4638cdb2eda754d43
chwyrligwgan <chwyrligwgan@qmail.com>
2013-11-20 21:07:49 +0400

    time format + setInterval

--- pavlova/scriptTime.js
+++ pavlova/scriptTime.js
@@ -1,3 +1,11 @@
-var cur = new Date();
+function getTime() {
+	var interval = setInterval(sayTime, 10000);
+}
 
-alert(cur.getHours() + ':' + cur.getMinutes() + ' ' + cur.getDay() + '.' + cur.getMonth() + ' ' + cur.getYear() + 'года' );
+function sayTime() {
+	var cur = new Date();
+
+	alert(cur.getHours() + ':' + cur.getMinutes() + ' ' + cur.getDay() + '.' + cur.getMonth() + ' ' + cur.getYear() + 'года');
+}
+
+getTime();
